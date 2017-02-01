1774e381149894bf4a00c4e9eadbefd5cb286a82
Orlando Masso <orlandomassoj@hotmail.com>
2017-01-31 21:07:45 -0500

    stabilization update

--- Reggie/js/functions.js
+++ Reggie/js/functions.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    $("#headline").fitText(1.2, { minFontSize: '14px', maxFontSize: '60px' });
+    $('#headline').fitText(1.2, { minFontSize: '14px', maxFontSize: '60px' });
 });
 
 
@@ -40,8 +40,62 @@ $( document ).ready(function() {
   };
 
 })( jQuery );
+//reggie resize header
 
 $( window ).scroll(function() {
  var val = $(window).scrollTop();
  $("#val").text(val);
 $("scrollreg").css({"background-position":val*2+"px 50%"}); });
+
+//menu switch
+
+var $document = $(document);
+
+$document.scroll(function() {
+  if ($document.scrollTop() >= 580) {
+    $("#nav").addClass("scroll-nav");
+    $("#nav").addClass("animated");
+    $("#nav").addClass("fadeInDown");
+    $("#nav").removeClass("default-nav");
+  }
+
+  else {
+    $("#nav").removeClass("scroll-nav");
+    $("#nav").removeClass("animated");
+    $("#nav").removeClass("fadeInDown");
+    $("#nav").addClass("default-nav");
+
+  }
+
+
+
+});
+
+//animations
+
+$document.scroll(function() {
+    //if ($(this).width() < 2000) {
+        if ($(this).height() <= 970) {
+            if ($document.scrollTop() < 300) {
+
+            $("#scrollbio").hide();
+            $("#scrollreg").hide();
+
+
+            }
+            if($(this).scrollTop() > 400) {
+
+            //web animations
+            $("#scrollbio").show();
+            $("#scrollreg").show();
+
+
+            $("#scrollreg").addClass("animated fadeInLeft");
+            $("#scrollbio").addClass("animated fadeInUp");
+
+
+
+            }
+      //    }
+        }
+});
